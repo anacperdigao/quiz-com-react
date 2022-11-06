5a44011b06024f464a6b324ae677a1a4445fdd86
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-11-06 11:55:06 -0300

    adding comments

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -6,8 +6,13 @@ import * as S from './styles'
 
 const Welcome = () => {
 
+  // O dispatch serve para eu poder entrar no reducer e executar o switch case que eu fiz lá.
+  // quizState eu pego os valores, e dispatch eu altero os valores.
   const [quizState, dispatch] = useContext(QuizContext)
 
+
+  // No onClick eu vou acessar o reducer através do dispatch e vou dizer qual o case que eu quero acessar no
+  // meu switch case, e nesse caso, vai ser o 'CHANGE_STATE'
   return (
     <S.WelcomeDiv>
         <S.Subtitle>Seja bem-vindo!</S.Subtitle>
--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -1,14 +1,17 @@
 import { createContext, useReducer } from "react";
 import questions from '../data/questions'
 
-
+// Determinando os estágios do jogo
 const STAGES = ['Start', 'Playing', 'End'];
 
+// Determinando qual o estado inicial quando o usuário entra na aplicação
 const initialStage = {
     gameStage: STAGES[0],
-    questions: questions
+    questions: questions,
 }
 
+// O reducer utiliza dois parâmetros, primeiro o estado inicial, e depois a ação que o usuario
+// vai fazer para modificar essa estado.
 const quizReducer = (state, action) => {
 
     switch (action.type) {
@@ -20,14 +23,17 @@ const quizReducer = (state, action) => {
     }
 }
 
+//--------------------------------------- Criação do contexto ---------------------------------------//
+
 export const QuizContext = createContext();
 QuizContext.displayName = 'quiz'
 
 
 export const QuizProvider = ({children}) => {
 
+    // No meu provider, vou passar o reducer através do value para poder prover onde eu quiser usar.
     const value = useReducer(quizReducer, initialStage);
-    
+
     return(
         <QuizContext.Provider value={value}>
             {children}
